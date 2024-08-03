3a365d5c0e89653ce7b8b5964bde90871302c532
Tuan Nguyen <anhtuann0210@gmail.com>
2024-08-03 21:55:12 +0700

    add QuickStartUi and update account controller using aspnet identity

--- src/IdentityServer/IdentityServer/Program.cs
+++ src/IdentityServer/IdentityServer/Program.cs
@@ -41,7 +41,16 @@ builder.Services.AddIdentityServer()
         options.ConfigureDbContext = b => b.UseSqlServer(defaultConnString, opts => opts.MigrationsAssembly(assembly));
     })
     .AddDeveloperSigningCredential();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
